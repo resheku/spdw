620995439b0540f55b469049f7bccb06cdaeb077
Resheku <5239900+resheku@users.noreply.github.com>
2024-02-20 15:10:01 +0900

    past-schedule workflow season 2022

--- scripts/schedule.sh
+++ scripts/schedule.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for i in 2023; do
+for i in {2022..2023}; do
     mkdir -p data/sel/$i
     echo https://stats.speedwayekstraliga.pl/api/v1/matches/schedule?s=$i
     curl -f --no-progress-meter https://stats.speedwayekstraliga.pl/api/v1/matches/schedule?s=$i -o data/sel/$i/schedule.json
