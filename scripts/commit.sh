9874f3b04d2057402ce5872bf920a132606cf3f9
Resheku <5239900+resheku@users.noreply.github.com>
2024-07-06 20:29:04 +0900

    updated workflow

--- scripts/commit.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-git config user.name "Automated"
-git config user.email "actions@users.noreply.github.com"
-git add data/
-timestamp=$(date -u)
-git commit -m "Latest data: ${timestamp}" && echo "changes=1" >> $GITHUB_OUTPUT
-git push
