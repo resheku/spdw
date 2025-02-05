78c8b0ec031956eecbd0153c1f3b84b268b10e22
Resheku <5239900+resheku@users.noreply.github.com>
2025-02-05 15:01:48 +0900

    fix schedule commit force

--- scripts/schedule_commit.sh
+++ scripts/schedule_commit.sh
@@ -5,7 +5,10 @@ git config user.email "actions@users.noreply.github.com"
 git add data/
 
 # Check for changes in JSON files if FORCE is not true
-if [ "$FORCE" != "true" ]; then
+if [ "$FORCE" = "true" ]; then
+    echo "Force commit"
+    echo "changes=1" >>"$GITHUB_OUTPUT"
+else
     json_changes=$(git diff --cached --name-only --diff-filter=ACM | grep '\.jsonl$')
     if [ -z "$json_changes" ]; then
         echo "No changes to commit"
