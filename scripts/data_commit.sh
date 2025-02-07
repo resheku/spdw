1432fe67c8aaf31d47e7aacd85e8f9e3796c0fd4
Resheku <5239900+resheku@users.noreply.github.com>
2025-02-07 22:45:02 +0900

    sel-data commit if changed to db

--- scripts/data_commit.sh
+++ scripts/data_commit.sh
@@ -4,12 +4,17 @@ git config user.name "Automated"
 git config user.email "actions@users.noreply.github.com"
 git add sel.db sel/
 
-# Check for changes in match JSON files
-json_changes=$(git diff --cached --name-only --diff-filter=ACM | grep 'matches.jsonl$')
-if [ -z "$json_changes" ]; then
-    echo "No changes."
-    echo "changes=0" >>"$GITHUB_OUTPUT"
-    exit 0
+# Check for changes in JSON files if FORCE is not true
+if [ "$FORCE" = "true" ]; then
+    echo "Force commit"
+    echo "changes=1" >>"$GITHUB_OUTPUT"
+else
+    json_changes=$(git diff --cached --name-only --diff-filter=ACM | grep '\.jsonl$')
+    if [ -z "$json_changes" ]; then
+        echo "No changes."
+        echo "changes=0" >>"$GITHUB_OUTPUT"
+        exit 0
+    fi
 fi
 
 timestamp=$(date -u)
