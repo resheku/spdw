7386579b42a4e6360d0438f8e69ce39aea8ea740
Resheku <5239900+resheku@users.noreply.github.com>
2025-12-16 17:52:18 +0900

    wip: spdw data match changes first commit

--- scripts/match_commit.sh
+++ scripts/match_commit.sh
@@ -4,13 +4,18 @@ git config user.name "Automated"
 git config user.email "actions@users.noreply.github.com"
 git add ${LEAGUE}/
 
-# Check for changes in JSON files
-json_changes=$(git diff --cached --name-only --diff-filter=ACM | grep '\.json$')
-if [ -z "$json_changes" ]; then
-    echo "No changes in JSON files"
-    exit 0
+# Check for changes in JSON files if FORCE is not true
+if [ "$FORCE" = "true" ]; then
+    echo "Force commit"
+else
+    # Check for changes in JSON files
+    json_changes=$(git diff --cached --name-only --diff-filter=ACM | grep '\.json$')
+    if [ -z "$json_changes" ]; then
+        echo "No changes in JSON files"
+        exit 0
+    fi
 fi
 
 timestamp=$(date -u)
-git commit -m "Match: ${timestamp}" || true
+git commit -m "Match ${LEAGUE}: ${timestamp}" || true
 git push
