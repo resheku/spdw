f8c44bac04b346b5d0b09a48182aa8f4d5475ed8
Resheku <5239900+resheku@users.noreply.github.com>
2025-12-16 18:40:06 +0900

    wip: spdw data sel2 workflow match commit

--- scripts/match_commit.sh
+++ scripts/match_commit.sh
@@ -2,7 +2,7 @@
 
 git config user.name "Automated"
 git config user.email "actions@users.noreply.github.com"
-git add ${LEAGUE}/
+git add "${LEAGUE}/"
 
 # Check for changes in JSON files if FORCE is not true
 if [ "$FORCE" = "true" ]; then
